b9352a31d6404643ee1c89b7bcd1979a0fec875c
Ryan Hagan <ryan@ryanhagan.net>
2020-09-15 20:38:06 -0400

    Game setup tests complete.

--- MonopolyKata/Monopoly.cs
+++ MonopolyKata/Monopoly.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace MonopolyKata
 {
     public class Monopoly
     {
+        public List<Player> Players { get; set; } = new List<Player>();
 
+        public Monopoly(List<Player> players)
+        {
+            Random random = new Random();
+            Players = players.OrderBy(a => random.Next()).ToList();
+        }
+
+        public void Play()
+        {
+            if (Players.Count() < 2 || Players.Count() > 8)
+                throw new InvalidOperationException("You must have at least 2 players and no more than 8 players!");
+        }
     }
 }
--- MonopolyKataTests/Monopolytests.cs
+++ MonopolyKataTests/Monopolytests.cs
@@ -1,4 +1,6 @@
 using MonopolyKata;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MonopolyKataTests
@@ -8,7 +10,58 @@ namespace MonopolyKataTests
         [Fact]
         public void Construct_Game_ShouldCreateGame()
         {
-            Monopoly game = new Monopoly();
+            Monopoly game = new Monopoly(
+                new List<Player>() { }
+                );
+        }
+        [Fact]
+        public void Setup_GameWith2Players_ShouldWork()
+        {
+            Player horse = new Player("Horse");
+            Player car = new Player("Car");
+
+            Monopoly game = new Monopoly(
+                new List<Player> { horse, car }
+                );
+            
+            Assert.Contains(horse, game.Players);
+            Assert.Contains(car, game.Players);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(9)]
+        [InlineData(15)]
+        public void Setup_GameWithWrongNumberOfPlayers_ShouldThrowException(int numPlayers)
+        {
+            List<Player> players = new List<Player>() {};
+
+            for (int i = 0; i < numPlayers; i++)
+                players.Add(new Player(i.ToString()));
+
+            Monopoly game = new Monopoly(players);
+
+            Assert.Throws<InvalidOperationException>(() => game.Play());
+        }
+
+        [Fact]
+        public void Setup_GameWith2Players_ShouldRandomizeOrder()
+        {
+            bool horseFirst = false;
+            bool carFirst = false;
+            Player horse = new Player("Horse");
+            Player car = new Player("Car");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Monopoly game = new Monopoly(
+                    new List<Player> { horse, car }
+                    );
+                if (game.Players[0].Name == "Horse") horseFirst = true;
+                if (game.Players[0].Name == "Car") carFirst = true;
+            }
+
+            Assert.True(horseFirst && carFirst);
         }
     }
 }
